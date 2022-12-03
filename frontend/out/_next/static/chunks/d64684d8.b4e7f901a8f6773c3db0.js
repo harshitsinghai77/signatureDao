b8e52131a8f0896a23afb31ab27c94a4f3a4a2c8
harshitsinghai77 <harshitsinghai77@gmail.com>
2022-12-03 19:29:46 +0530

    Add frontend and contract

--- frontend/out/_next/static/chunks/d64684d8.b4e7f901a8f6773c3db0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[9],{PRV2:function(n,t,o){"use strict";o.d(t,"a",(function(){return e}));var r=o("Lnxd");function e(n){return Object(r.a)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M9 5l7 7-7 7"}}]})(n)}}}]);
